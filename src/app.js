0a4d893b7edb3bb5822fdc5506db02ae64335f9e
WheatleyTheCoder <slendermanwithaface@gmail.com>
2018-04-03 18:25:29 +0100

    Added all the files

---
+++ src/app.js
@@ -0,0 +1,1 @@
+console.log(`Did you know that "1+1" actually equals ${ 1+1 }?`);
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+var path = require('path');
+var webpack = require('webpack');
+
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin()
+    ]
+};
